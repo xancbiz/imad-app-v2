0e85abec8bed626bf9f24300499cb69b2441b4d1
xancbiz <xancbiz@gmail.com>
2017-02-14 13:31:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var morgan = require('morgan');
+var express = require('express'); 
+var morgan = require('morgan'); "logs"
 var path = require('path');
 
 var app = express();
