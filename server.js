c3a26f2ddef75d90b8ed33b1b285a776d09457c8
xancbiz <xancbiz@gmail.com>
2017-02-17 10:36:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,6 +6,40 @@ var app = express();
 app.use(morgan('combined'));
 var articles = {}
 function createTemplate (data) {}
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    //var htmlTemplate = '
+       <html>
+    <head>
+        <title>
+            $(title)
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href = "ui/style.css" ref = "stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/"> Home is where you hang your hat </a>
+        </div>
+        <div> 
+        <hr>
+        <hdg>
+        $(heading)
+        </hdg>
+        <div>
+            $(date)
+        </div>
+        <div>
+        $(content)
+        </div>
+    </body>
+</html>
+         
+    return htmlTemplate;
+    }
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
