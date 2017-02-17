c64f13141beae21765d47bc5e290e219fbab82a3
xancbiz <xancbiz@gmail.com>
2017-02-17 16:30:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,40 +4,62 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var articles = {}
+
+
+var articles = {
+articleOne : {
+    title: 'Article One - Sushil',
+    heading: 'Article One',
+    date: '17 Feb 2017',
+    content: ' This is the content for Article One. This is the content for Article One. This is the content for Article One. This is the content for Article One. This is the content for Article One. This is the content for Article One. This is the content for Article One. This is the content for Article One.'
+    },
+articleTwo : {
+    title: 'Article two - Sushil',
+    heading: 'Article Two',
+    date: '18 Feb 2017',
+    content: 'This is the content for Article Two. This is the content for Article Two. This is the content for Article Two. This is the content for Article Two.This is the content for Article Two. This is the content for Article Two. This is the content for Article Two. This is the content for Article Two.'
+    },
+    articleThree : {
+    title: 'Article Three - Sushil',
+    heading: 'Article Three',
+    date: '19 Feb 2017',
+    content: 'This is the content for Article Three. This is the content for Article Three. This is the content for Article Three. This is the content for Article Three.This is the content for Article Three. This is the content for Article Three. This is the content for Article Three. This is the content for Article Three.'
+    },
+};
 function createTemplate (data) {}
     var title = data.title;
     var date = data.date;
     var heading = data.heading;
     var content = data.content;
-    //var htmlTemplate = '
-       <html>
-    <head>
-        <title>
-            $(title)
-        </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href = "ui/style.css" ref = "stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-        <div>
-            <a href="/"> Home is where you hang your hat </a>
-        </div>
-        <div> 
-        <hr>
-        <hdg>
-        $(heading)
-        </hdg>
-        <div>
-            $(date)
-        </div>
-        <div>
-        $(content)
-        </div>
-    </body>
+    var htmlTemplate = '
+    <html>
+        <head>
+            <title>
+                $(title)
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href = "ui/style.css" ref = "stylesheet" />
+        </head>
+            <body>
+                <div class="container">
+                    <div>
+                        <a href="/"> Home is where you hang your hat </a>
+                    </div>
+                    <div> 
+                    <hr>
+                    <hdg>
+                    $(heading)
+                    </hdg>
+                    <div>
+                        $(date)
+                    </div>
+                    <div>
+                    $(content)
+                    </div>
+                </div>
+            </body>
 </html>
-         
+   ';      
     return htmlTemplate;
     }
 
@@ -46,7 +68,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.sendFile(createTemplate (articleOne));
 });
 
 app.get('/articleName', function (req, res) {
