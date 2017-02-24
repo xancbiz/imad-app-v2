208c8c18991132564887d13d7bafc771e2c1e5f0
xancbiz <xancbiz@gmail.com>
2017-02-24 23:14:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,30 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+var pool = new Pool(config);
+pool.query('SELECT* FROM test', function(err, result) {
+      // handle an error from the query
+      if(err) {
+          res.status(500), send(error.tostring())
+      } else {
+          res.send(JSON.stringlfy(result));
+      }
+      });
+      
+      
+app.get ('/test-db', function (req, res) {
+    //make a SELECT request and
+    // return a response with data
 });
 
 app.get('/ui/main.js', function (req, res) {
